f790d53d4b15d8b1ab3921cd0b0cc2fc5d2d1d99
personqianduixue <3480814463@qq.com>
2023-05-08 16:08:21 +0800

    update

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 year = 23
 total_num = 30000
+# 进程数根据CPU和内存能力设置
 # step=30000/进程数
 #开60进程
 download_step = 500
 #开60进程
-
 pdf2text_step = 500
 #你的学校
 your_university= 'Huazhong University of Science and Technology'
